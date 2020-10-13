9b253992a722acba2a38aeac5fad6a5d15f06623
Chelsey <cswilik@gmail.com>
2020-10-13 20:25:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,8 +2,32 @@
 def game_hash 
   b_ball_game = {
   :home => {
-    :team_name => "team name",
-    :colors => [ "color1", "color2" ],
+    :team_name => "Brooklyn Nets",
+    :colors => [ "Black", "White" ],
+    :players => [{
+      :player_name => "Alan Anderson",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1
+    },
+      {:player_name => "Reggie Evans"
+      :number => 30,
+      :shoe => 14,
+      :points => 12,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 12,
+      :blocks => 12,
+      :slam_dunks => 7
+    }]},
+  :away => {
+    :team_name => "Charlotte Hornets",
+    :colors => ["Turquoise", "Purple"],
     :players => [{
       :player_name,
       :number,
@@ -13,12 +37,8 @@ def game_hash
       :assists,
       :steals,
       :blocks,
-      :slam
-    }]},
-  :away => {
-    :team_name => "team name",
-    :colors => ["color1", "color2"],
-    :players => [{}] }
+      :slam_dunks
+    }] }
   }
   
   
