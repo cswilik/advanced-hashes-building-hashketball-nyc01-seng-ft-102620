74d76d3df36a6967c5f35b933f02e38ae2784f69
Chelsey <cswilik@gmail.com>
2020-10-13 20:14:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,13 @@
 # Write your code here!
 def game_hash 
-  b_ball_game 
+  b_ball_game = {:home => {
+    :team_name,
+    :colors,
+    :players },
+  :away => {
+    :team_name,
+    :colors,
+    :players }
+  }
+  
+  
